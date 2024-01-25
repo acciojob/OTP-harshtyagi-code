cea4dac780ef2030e69a0bfec918a762eb57a497
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-25 10:25:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+return ;
+		}
 		
+		
+		
+		if(code>=48 && code<=57){
+			let nextElement = currentElement.nextElementSibling ;
+			if(nextElement){
+				nextElement.focus() ;
+			}
+			
+		}else{
+			event.target.value="" ;
+		}
+	})
+	
+}
